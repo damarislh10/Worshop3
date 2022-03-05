f2e108c823786bffbd62e94282d2ec785f0e2410
anyelithj <anyelithj@misena.edu.co>
2022-03-04 21:14:00 -0500

    firebase

--- workshop-libreria/src/routers/AppRouters.js
+++ workshop-libreria/src/routers/AppRouters.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from '../components/Login';
+import Registro from '../components/Registro';
 
 function AppRouters() {
   return (
     <BrowserRouter>
      <Routes>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/registro' element={<Registro/>}/>
      </Routes>
     </BrowserRouter>
   );
