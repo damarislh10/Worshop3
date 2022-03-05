b6c7d59e030565dad3947e3b821d9218770e3c18
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-03-04 20:47:15 -0500

    Structure listar in actions edit

--- workshop-libreria/src/components/Editar.js
+++ workshop-libreria/src/components/Editar.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Modal } from "bootstrap";
+import { Button } from "bootstrap";
+import React from "react";
+import { Form } from "react-bootstrap";
 
 const Editar = () => {
   return (
     <div>
-      <h2>Editar</h2>
-    </div>
-  )
-}
+      <Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Tarea</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Nombre de la Tarea</Form.Label>
+              <Form.Control
+                type="text"
+                name="tarea"
+                placeholder="Ingrese nombre tarea"
+              />
+            </Form.Group>
 
-export default Editar
+            <Button variant="secondary">Close</Button>
+            <Button type="submit" variant="primary">
+              Save Changes
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default Editar;
